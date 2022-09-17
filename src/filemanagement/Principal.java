5c164cb34fe47b7a5edc259c5d779e931dfe4492
A59-1-1022-015 <A59-1-1022-015@DESKTOP-V1JECNS>
2022-09-16 20:29:07 -0500

    Se modifica el nombre de la clase

---
+++ src/filemanagement/Principal.java
@@ -0,0 +1,20 @@
+
+package filemanagement;
+
+/**
+ *
+ * @author A59-1-1022-015
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Principal file_1 = new Principal();
+       
+       file_1.cre
+       
+    }
+    
+}
