df32578fa5980519698f6359c2b4c3165b3345e7
A59-1-1022-015 <A59-1-1022-015@DESKTOP-V1JECNS>
2022-09-16 19:19:17 -0500

    Creacion del proyecto

---
+++ src/filemanagement/FileManagement.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package filemanagement;
+
+/**
+ *
+ * @author A59-1-1022-015
+ */
+public class FileManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
