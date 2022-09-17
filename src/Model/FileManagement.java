5109b0f5f8a19a62b2bb0b55f86c086375cb81fd
A59-1-1022-015 <A59-1-1022-015@DESKTOP-V1JECNS>
2022-09-16 20:03:02 -0500

    Creación del método create_file

---
+++ src/Model/FileManagement.java
@@ -0,0 +1,51 @@
+
+package Model;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class FileManagement {
+    
+    
+    private File file;
+    private FileWriter write_file;
+    private String name_file;
+
+    public FileManagement() {
+        this.name_file = "";
+        this.file = null;
+        this.write_file = null;
+        
+    }
+
+    public String getName_file() {
+        return name_file;
+    }
+
+    public void setName_file(String name_file) {
+        this.name_file = name_file;
+    }
+    
+    public String create_file(String name_file){
+        setName_file(name_file);
+        String message = "";
+        this.file = new File(getName_file());
+        if(!this.file.exists()){
+            try {
+                this.file.createNewFile();
+                message = "Archivo creado";
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }else{
+            message="El archivo ya existe";
+        }
+        return message;
+    }
+    
+    
+    
+    
+}
