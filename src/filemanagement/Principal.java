5ff784b6012e2a01cc44d4dcc13a471c4d7e3aed
A59-1-1022-015 <A59-1-1022-015@DESKTOP-V1JECNS>
2022-09-21 20:06:22 -0500

    Se llama el método append_to_file para validar el funcionamiento

--- src/filemanagement/Principal.java
+++ src/filemanagement/Principal.java
@@ -1,6 +1,7 @@
 
 package filemanagement;
 import Model.FileManagement;
+import java.io.IOException;
 /**
  *
  * @author A59-1-1022-015
@@ -9,10 +10,12 @@ public class Principal {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
        FileManagement file_1 = new FileManagement();
         System.out.println(file_1.create_file("Ejemplo.txt"));
+        file_1.append_to_file("\n Hola Clase Insertando segunda entrada de datos");
     }
     
 }
